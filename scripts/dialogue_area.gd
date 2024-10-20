ff232aee6c6ef30db9b2e53f51f1654d8f86cfb6
maz8569 <akarpetskiy82@gmail.com>
2024-10-20 13:29:51 +0200

    text labels above dialogue areas

--- scripts/dialogue_area.gd
+++ scripts/dialogue_area.gd
@@ -1,7 +1,14 @@
 extends Area2D
 
 @export var dialogue_key = ""
+@export var area_name = ""
 var area_active = false
+
+@onready var label: Label = %AreaLabel
+
+func _ready() -> void:
+	label.visible = false
+	label.text = area_name
 
 func _input(event):
 	if area_active and event.is_action_pressed("interaction"):
@@ -10,7 +17,11 @@ func _input(event):
 
 func _on_area_entered(area: Area2D) -> void:
 	area_active = true
+	if !area_name.is_empty():
+		label.visible = true
 
 
 func _on_area_exited(area: Area2D) -> void:
 	area_active = false
+	if !area_name.is_empty():
+		label.visible = false
