9cd9628c6cfb3ae62a671362770146bddc6f8495
Eder Santander <eder.santander.123@gmail.com>
2024-10-20 08:11:53 -0500

    Setup ghost sprite for player

--- scripts/player.gd
+++ scripts/player.gd
@@ -5,8 +5,6 @@ extends CharacterBody2D
 @onready var mansion_camera: Camera2D = $mansion_camera
 @onready var health_bar: ProgressBar = $health_bar
 
-var enemy_in_attack_range: bool = false
-var enemy_attack_cooldown: bool = true
 var health: int = 100
 var player_alive: bool = true
 var attack_state: bool = false
@@ -24,12 +22,11 @@ const SPEED: float = 100
 var direction: int = 0 #means no direction
 var previous_direction: int = 4
 var previous_velocity: Vector2 = Vector2(0,0)
-var previous_idle_animation: String = "front_idle"
+var previous_idle_animation: String = "ghost_front_idle"
 var enemy: CharacterBody2D = null
 
 func _physics_process(delta: float) -> void:
 	player_movement(delta)
-	enemy_attack()
 	current_camera()
 	update_health()
 	
@@ -58,9 +55,6 @@ func player_movement(delta: float):
 	move_and_slide()
 	previous_velocity = velocity
 	
-	attack_state = Input.is_action_just_released("attack")	
-	play_attack_animation(previous_direction)
-	
 	interact_state = Input.is_action_just_released("interaction")
 	if interact_state:
 		handle_interaction()
@@ -75,54 +69,35 @@ func handle_interaction():
 		block_object.interaction_with_player()
 	
 
-func play_attack_animation(direction: int):
-	if attack_state:
-		Global.player_current_attack = true
-		if direction == 1:
-			animated_sprite.flip_h = false
-			animated_sprite.play("side_attack")
-		elif direction == 2:
-			animated_sprite.flip_h = true
-			animated_sprite.play("side_attack")
-		elif direction == 3:
-			animated_sprite.play("back_attack")
-		elif direction == 4:
-			animated_sprite.play("front_attack")
-		# Handle damage to enemy
-		if enemy!= null && enemy_in_attack_range:
-			enemy.handle_damage()
-		animated_sprite.animation_finished.connect(attack_animation_finished)
-
-
 func play_direction_animation(direction: int, previous_velocity: Vector2):
 	# Region to handle walking
 	if direction == 1 || direction == 2:
 		if velocity.x > 0:
 			animated_sprite.flip_h = false
-			animated_sprite.play("side_walk")
+			animated_sprite.play("ghost_side_walk")
 		elif velocity.x < 0:
 			animated_sprite.flip_h = true
-			animated_sprite.play("side_walk")
+			animated_sprite.play("ghost_side_walk")
 	elif direction == 3 || direction == 4:
 		if velocity.y > 0:			
-			animated_sprite.play("front_walk")
+			animated_sprite.play("ghost_front_walk")
 		elif velocity.y < 0:			
-			animated_sprite.play("back_walk")
+			animated_sprite.play("ghost_back_walk")
 
 	# Region to handle idle
 	if direction == 0 && !attack_state:
-		var anim_to_play: String = "front_idle"
+		var anim_to_play: String = "ghost_front_idle"
 		if  (abs(previous_velocity.x)!=0 || abs(previous_velocity.y)!=0):
 			if previous_velocity.x > 0:
 				animated_sprite.flip_h = false
-				anim_to_play = "side_idle"
+				anim_to_play = "ghost_side_idle"
 			elif previous_velocity.x < 0:
 				animated_sprite.flip_h = true
-				anim_to_play = "side_idle"
+				anim_to_play = "ghost_side_idle"
 			elif previous_velocity.y < 0:
-				anim_to_play = "back_idle"
+				anim_to_play = "ghost_back_idle"
 			elif previous_velocity.y > 0:
-				anim_to_play = "front_idle"
+				anim_to_play = "ghost_front_idle"
 			previous_idle_animation = anim_to_play
 			animated_sprite.play(anim_to_play)
 
@@ -131,37 +106,10 @@ func player():
 	pass
 
 
-func _on_player_hitbox_body_entered(body: Node2D) -> void:
-	if body.has_method("enemy"):
-		enemy_in_attack_range = true
-		enemy = body
-
-
 func _on_player_hitbox_body_exited(body: Node2D) -> void:
-	if body.has_method("enemy"):
-		enemy_in_attack_range = false
-		enemy = null
-	elif body.has_method("interactable"):
+	if body.has_method("interactable"):
 		interactable_in_range = false
 		interactable_object = null
-
-
-func enemy_attack():
-	if enemy_in_attack_range and enemy_attack_cooldown:
-		health -= 5
-		enemy_attack_cooldown = false
-		timer_enemy_cooldown.start()
-
-
-func _on_attack_cooldown_timeout() -> void:
-	enemy_attack_cooldown = true
-
-
-func attack_animation_finished():
-	attack_state = false
-	Global.player_current_attack = false
-	animated_sprite.stop()
-	animated_sprite.play(previous_idle_animation)
 
 
 func current_camera():
