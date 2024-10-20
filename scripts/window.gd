f8d7055c99494a69b841944a3fa5259e9e107a4d
Eder Santander <eder.santander.123@gmail.com>
2024-10-20 18:37:50 -0500

    Added windows to the mansion scene

--- scripts/window.gd
+++ scripts/window.gd
@@ -2,7 +2,16 @@ extends Area2D
 @onready var window_sound: AudioStreamPlayer2D = $window_sound
 var is_player_inside:bool = false
 var sound_position:float = 0.0
+var percentage_volume:float = 0.0
+var player_body: Node2D = null
 
+var max_volume:float = 80.0
+
+func _process(delta: float) -> void:
+	if is_player_inside && player_body!=null:
+		var distance:float = (self.position - player_body.position).length()
+		percentage_volume = (max_volume - distance)/max_volume;
+		window_sound.volume_db = linear_to_db(percentage_volume)
 
 func _on_ready() -> void:
 	window_sound.stream = Global.SOUND_ambience_rain_window
@@ -11,8 +20,8 @@ func _on_ready() -> void:
 
 func _on_body_entered(body: Node2D) -> void:
 	if body.has_method("player"):
-		print("window")
 		is_player_inside = true
+		player_body = body
 		if sound_position > window_sound.stream.get_length():
 			sound_position = 0.0
 		window_sound.play(sound_position)
@@ -20,7 +29,7 @@ func _on_body_entered(body: Node2D) -> void:
 
 func _on_body_exited(body: Node2D) -> void:
 	if body.has_method("player"):
-		print("OUT window")
 		sound_position = window_sound.get_playback_position()
 		window_sound.stop()
 		is_player_inside = false
+		player_body = null
